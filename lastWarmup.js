f5c4b4a57e85d3b0ef6af2b06bcb2b1c21da2c8e
Mfelokour <Mfelokour@gmail.com>
2019-03-27 09:40:21 +0200

    :D

--- lastWarmup.js
+++ lastWarmup.js
@@ -7,4 +7,22 @@ var nums = [2, 7, 11, 15], target = 9,
 twoSum(nums,9); //should return [0,1] because nums[0] + nums[1] = 2 + 7 = 9;
 */
 
+function twoSum(nums,target){
+	var indexArr = [];
+	var init = nums[0]
+	var initIndex = 0
+	for (var i = 1; i < nums.length; i++) {
+	 	if (init + nums[i] === target){
+	 	indexArr.push(initIndex);
+	 	indexArr.push(i);
+	 	};
+	 	else if (init + nums[i] !== target){
+	 	init = nums[i];
+	 	initIndex++
+	 	};
+
+	};
+	return indexArr;
+}
+
 
